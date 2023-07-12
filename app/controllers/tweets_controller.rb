6deed6b74652c89e47336713ae7b910c93056349
Bárbara <barmunozs@gmail.com>
2023-07-11 22:55:42 -0400

    Se agregan estilos y funcionalidades

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -24,44 +24,37 @@ class TweetsController < ApplicationController
 
   # GET /tweets/1/edit
   def edit
+    @tweet = Tweet.find(params[:id])
   end
 
   # POST /tweets or /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
-
-    respond_to do |format|
-      if @tweet.save
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully created." }
-        format.json { render :show, status: :created, location: @tweet }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
+  
+    if @tweet.save
+      redirect_to tweets_path, notice: "Tu Tweet ha sido creado con éxito."
+    else
+      render :new
     end
   end
+  
+  
 
   # PATCH/PUT /tweets/1 or /tweets/1.json
   def update
-    respond_to do |format|
-      if @tweet.update(tweet_params)
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully updated." }
-        format.json { render :show, status: :ok, location: @tweet }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: 'Tweet actualizado correctamente.'
+    else
+      render :edit
     end
   end
+  
 
   # DELETE /tweets/1 or /tweets/1.json
   def destroy
     @tweet.destroy
-
-    respond_to do |format|
-      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to tweets_path, notice: "Tweet eliminado con éxito"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   resources :tweets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  #get "/inicio", to: "tuits#index"
-  post "/crear", to: "tweets#create"
-  patch "/actualizar", to: "tweets#update"
+
 
   # Defines the root path route ("/")
   root "tweets#index"
